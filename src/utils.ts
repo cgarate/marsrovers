28249c8aeefce0e33e0e7b5a45ecd0c875ce20c4
Carlos Garate <carlos.m.garate@gmail.com>
2020-12-16 00:56:03 -0500

    add comments to util functions

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,18 @@
-import { OrientationEnum, OrientationStringTypes, SideEnum, SideStringTypes } from "./rover.types";
+import {
+  OrientationEnum,
+  OrientationStringTypes,
+  SideEnum,
+  SideStringTypes,
+} from "./rover.types";
 
+// This function will advance positively or negatively the rover in the corresponding axis
 export const moveRover = (
   orientation: OrientationStringTypes,
   currentX: number,
-  currentY: number
+  currentY: number,
 ) => {
   const coordinates = { x: currentX, y: currentY };
+  // Orientation defines the axis we are moving on and the sign applied to the operation
   switch (orientation) {
     case "N":
       return { ...coordinates, y: currentY + 1 };
@@ -20,16 +27,20 @@ export const moveRover = (
   }
 };
 
+// By assigning values to each orientation we can rotate the rover by adding the value of the current orientation to the new orientation
 export const rotateRover = (
   orientation: OrientationStringTypes,
-  rotateTo: SideStringTypes
+  rotateTo: SideStringTypes,
 ) => {
+  // Since we set N to be 360 we adjust for this one manually
   if (orientation === "N" && rotateTo === "R") {
     return "E";
   }
   let newPositionDegrees = OrientationEnum[orientation] + SideEnum[rotateTo];
+
+  // Same here, if we are back to 0, we know it's N and its matched value should be 360
   if (newPositionDegrees === 0) {
     newPositionDegrees = 360;
   }
-  return OrientationEnum[newPositionDegrees];
+  return OrientationEnum[newPositionDegrees] as OrientationStringTypes;
 };
