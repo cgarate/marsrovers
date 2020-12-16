83453fa6ac9c0393557104be03a3d7d6eecd01e6
Carlos Garate <carlos.m.garate@gmail.com>
2020-12-16 01:37:33 -0500

    add types for instructions

--- src/rover.types.ts
+++ src/rover.types.ts
@@ -2,12 +2,22 @@ export enum OrientationEnum {
   N = 360,
   E = 90,
   S = 180,
-  W = 270
+  W = 270,
 }
 export type OrientationStringTypes = keyof typeof OrientationEnum;
 
 export enum SideEnum {
   L = -90,
-  R = +90
+  R = +90,
 }
 export type SideStringTypes = keyof typeof SideEnum;
+
+export interface InstructionsType {
+  plateauSizeX?: number;
+  plateauSizeY?: number;
+  roverName?: string;
+  landingX?: number;
+  landingY?: number;
+  orientation?: string;
+  instructionSet?: string[];
+}
